a57edc07a9a29ce7d9693d67f4aeead1dc629f83
Deanna Warren <mrs.deanna.warren@gmail.com>
2015-10-07 13:53:14 -0700

    Fix readability 5.2 reflection

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -90,15 +90,26 @@ end
 
 --Were you successful in breaking the problem down into small steps?
   I felt pretty happy with my pseudocode
---Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
-  It was pretty easy to make into code, although I did have a moment of doubt about how to make the destructive vs non-destructive numbers.
---Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
-  it wasn't successful in passing all of the tests. we had some trouble with passing the array so that there wasn't a nested array.
---When you refactored, did you find any existing methods in Ruby to clean up your code?
-  refactoring actually lessened the methods! We were able to remove flatten by making new_array equal to the original array.
+--Once you had written your pseudocode, were you able to easily translate it into
+ code? What difficulties and successes did you have?
+  It was pretty easy to make into code, although I did have a moment of doubt
+  about how to make the destructive vs non-destructive numbers.
+--Was your initial solution successful at passing the tests? If so, why do you
+think that is? If not, what were the errors you encountered and what did you do
+to resolve them?
+  it wasn't successful in passing all of the tests. we had some trouble with
+  passing the array so that there wasn't a nested array.
+--When you refactored, did you find any existing methods in Ruby to clean up your
+code?
+  refactoring actually lessened the methods! We were able to remove flatten by
+  making new_array equal to the original array.
 --How readable is your solution? Did you and your pair choose descriptive variable names?
-  I think our variable names are descriptive, and the code seems pretty readable, but that might be my having had a hand in writing it!
---What is the difference between destructive and non-destructive methods in your own words?
-  A destructive method changes the value of what it is passed to it. A non-destructive method takes the value and outputs a changed version without changing the original value.
+  I think our variable names are descriptive, and the code seems pretty readable,
+   but that might be my having had a hand in writing it!
+--What is the difference between destructive and non-destructive methods in your
+own words?
+  A destructive method changes the value of what it is passed to it. A non-
+  destructive method takes the value and outputs a changed version without
+  changing the original value.
 
 =end
