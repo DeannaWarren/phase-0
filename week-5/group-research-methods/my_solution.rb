247dcb49f9ca817e22bc72f7f36abe90234657cb
Deanna Warren <mrs.deanna.warren@gmail.com>
2015-10-09 21:20:06 -0700

    Add Group research project

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -1,5 +1,3 @@
-require "rspec/autorun"
-
 # Research Methods
 
 # I spent [] hours on this challenge.
@@ -10,59 +8,68 @@ my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, 
 # Person 2
 
 def my_array_modification_method!(source, thing_to_modify)
-
+  message = Array.new
+  source.each do |x|
+    if x.is_a?(Numeric)
+      message << x.to_i + thing_to_modify.to_i
+    elsif
+      message << x
+    end
   end
+  source.replace(message)
+  return source
 end
 
 
 def my_hash_modification_method!(source, thing_to_modify)
+  source.each do |key,value|
+    source[key] = value + thing_to_modify
+  end
+  p source
 end
 
-my_array_modification_method!(i_want_pets,1)
 
 # Release 1: Identify and describe the Ruby method you implemented. Teach your
 # accountability group how to use the methods.
-# .is_a?
-#
-#
 
+# -- .each --
+#   .each is a way to itterate an array or hash for each value inside it.
+#   placing it on the array/hash itself, or on a variable/local variable name
+#   for one, allows it to be called. it's called with an element in this fashion:
+#       array.each do |element|
+#         ### do something with element here
+#       end
+#   and that's how you use .each.
+
+# --.is_a?()--
+#   I'm going to start by passing you a link, b/c I still don't really understand
+#   all of this:
+#       http://stackoverflow.com/questions/10558504/can-someone-explain-the-class-superclass-class-superclass-paradox
+#   So the basic conceit of .is_a?(Class) is  to ask an element if it's a certain
+#   type of thing/ member of a specific class. I used Numeric because it's a
+#   Superclass containing the classes of float and integer, but I'm not super
+#   comfortable with that concept yet. Just know you can ask if something is
+#   in a certain class, and so far Numeric (or Integer or Float) and String
+#   are the most relevent.
+#   Oh, it returns boolean, that's why I called it as the argument on an if
+#   statement.
+
+# --.to_i--
+#     we've covered .to_i before, it attempts to turn the object handed to it
+#     into an integer. it can convert a float into an integer, and a string
+#     ainto an integer if that string contains only numbers. If the string
+#     contains numbers and number characters it will parse together an
+#     output of the numbers (ignoring any whitespace) until the first
+#     non-numeric character and then stops parsing. Any numerics
+#     after the non-numeric will be ignored.
+
+
+# --.replace--
+#     This takes the hash/array that it is called on and replaces it's
+#     contents with the contents of an array that is the argument. The
+#     original values are destroyed.
 
 # Release 3: Reflect!
 # What did you learn about researching and explaining your research to others?
 #
-#
-#
-#
-
-
-#-------------------
-
-#=begin
-# PERSON 2: MODIFY EXISTING DATA
-describe 'PERSON 2: my_array_modification_method!' do
-  let(:i_want_pets) {["I", "want", 3, "pets", "but", "only", "have", 2 ]}
-
-  it "adds one to any integer in the array" do
-    expect(my_array_modification_method!(i_want_pets, 1)).to eq ["I", "want", 4, "pets", "but", "only", "have", 3 ]
-  end
-
-  it "operates destructively by altering the original array" do
-     expect(my_array_modification_method!(i_want_pets, 1).object_id).to eq(i_want_pets.object_id)
-  end
-
-end
-
-describe 'PERSON 2: my_hash_modification_method!' do
-
-  let(:my_family_pets_ages) {{"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0, "Ditto" => 3}}
-
-  it "adds a specified number of years to any integer in the hash" do
-    expect(my_hash_modification_method!(my_family_pets_ages, 2)).to eq({"Evi" => 8, "Hoobie" => 5, "George" => 14, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5})
-  end
-
-  it "operates destructively by altering the original hash" do
-     expect(my_hash_modification_method!(my_family_pets_ages, 2).object_id).to eq(my_family_pets_ages.object_id)
-  end
-
-end
-#=end
+# researching is hard, but it's easier when you know what specific you're researching and the extra research to teach helps me understand what i'm using.
