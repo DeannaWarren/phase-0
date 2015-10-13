0a56ebcd8c79d4103ce9e2779e18a760bf4121e1
Deanna Warren <mrs.deanna.warren@gmail.com>
2015-10-12 19:26:25 -0700

    Add completed GPS file

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -4,30 +4,35 @@
 
 # We spent [#] hours on this challenge.
 
+# 1. Variable Names X
+# 2. Refactor argument Error on 29
+# 3. Refactor error counter on 23
+# 4. Refactor values_at on 33
+# 5.
+
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, order_quantity)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+#Hash defining food as keys and serving size as values
+  menu = {"cookie" => 1, "cake" =>  5, "pie" => 7}
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
-    end
-  end
+#Iterates through library checking to see if items to make are not equal to items in library. Error counter should return either 1 or 0
 
-  if error_counter > 0
+  if menu.has_key?(item_to_make) == false
     raise ArgumentError.new("#{item_to_make} is not a valid input")
   end
 
-  serving_size = library.values_at(item_to_make)[0]
-  serving_size_mod = order_quantity % serving_size
+#Setting up whether or not you have an even number to the serving size.
+  serving_size = menu[item_to_make]
+#Returns a remainder after diving order quantity by serving size calculating leftover ingredients
+  leftover_ingredients = order_quantity % serving_size
 
-  case serving_size_mod
+#Returns notification of quantity of items needed and any remaining ingredients if available.
+  case leftover_ingredients
   when 0
     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
   else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{leftover_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
   end
 end
 
@@ -41,3 +46,11 @@ p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
 
+# What did you learn about making code readable by working on this challenge?
+#   Variable names are important for readability. Counters aren't always needed (even if you love them very much like me.) Challenges about debugging or refacoring others work is very much rife with "But why would you do that?!"
+# Did you learn any new methods? What did you learn about them?
+#   I learned about values_at. It was a bit confusing, but really I just didn't like it. there are other ways to get at the same thing. However, if that's what you need it's there.
+# What did you learn about accessing data in hashes?
+#   I solidified the syntax of calling values using keys. I'm not unfamiliar with it but I almost always have to double check that it works.
+# What concepts were solidified when working through this challenge?
+#   See above. Also that there's a huge amount of possible answers to almost anything. And some of those aren't very pretty even when they work, so fixing those is needed when possible.
