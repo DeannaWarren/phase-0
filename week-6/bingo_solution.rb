43a4472dfde0ba53d00ad11a23eed05da5d4df6c
Deanna Warren <mrs.deanna.warren@gmail.com>
2015-10-17 08:50:43 -0700

    Add Attr/Credit Card and Solo Challenges

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -2,33 +2,106 @@
 
 # I spent [#] hours on this challenge.
 
+=begin
+TABLE OF CONTENTS (so much code)
+Pseudocode - line X
+Initial Solution- line X
+Refactored Solution- line X
+Reflection- line X
+=end
 
 # Release 0: Pseudocode
 # Outline:
 
 # Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
-  #fill in the outline here
+  #call the random method
+  # choose a letter from a list of letters (b i n g o) using random
+  # choose a number from a range 1-100 using random
 
 # Check the called column for the number called.
-  #fill in the outline here
+  #for each item in the array (so each array within the main one)
+    # check the item at index equal to the letter ie 0 for b ect
 
 # If the number is in the column, replace with an 'x'
-  #fill in the outline here
+  # transform found number into string and replace with "x"
 
+#REMOVED BELOW DISPLAY BECAUSE I DON'T KNOW WHY IT'S THERE
+#IT'S NOT IN THE INSTRUCITONS WHY IS IT HERE
 # Display a column to the console
-  #fill in the outline here
+  #look at the letter pertaining to the column
+  #display the appropriate index from each inner hash
 
 # Display the board to the console (prettily)
-  #fill in the outline here
+  #for each index between 0 and 4, print inner list on new line
 
 # Initial Solution
 
 class BingoBoard
 
-  def initialize(board)
+  def initialize(board) # creates new bingo board
     @bingo_board = board
+    #add relevent variables below
+    @B = ["B",0]
+    @I = ["I",1]
+    @N = ["N",2]
+    @G = ["G",3]
+    @O = ["O",4]
+    @letters = [@B,@I,@N,@G,@O]
   end
 
+  def caller# Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
+    bucket = Random.new #call the random method
+    @call = Array.new
+    @call << [@B,0]#@letters[bucket.rand(0..4)]# choose a letter
+    @call << 44#bucket.rand(1..100)# choose a number
+    puts "caller"
+    p @call
+    p "#{@call[0][0]}, #{@call[1]}"
+  end
+
+  def checker# Check the called column for the number called.
+    puts "checker"
+    column = @call[0][1] # column = index matched to letter
+    number = @call[1] #number to look for
+    @correct_tile = Array.new
+    for row in 0..4 do#for each inner array
+      puts "checking #{row},#{column}"
+      # check the item at index equal to the letter below
+       if @bingo_board[row][column] == number
+        @correct_tile << row
+        @correct_tile << column
+        puts "true for #{@correct_tile}"
+      end
+    end
+    if @correct_tile == []
+      @correct_tile << "Not Found"
+    end
+    p @correct_tile
+   end
+
+  def mark_correct# If the number is in the column, replace with an 'x'
+  # transform found number into string and replace with "x"
+    if @correct_tile != "Not Found"
+      p @bingo_board[@correct_tile[0]][@correct_tile[1]] = " X"
+    end
+  end
+
+   def method_next# DO NOT DO THE THING
+    # Display a column to the console
+  #look at the letter pertaining to the column
+  #display the appropriate index from each inner hash
+   end
+
+   def method# Display the board to the console (prettily)
+    for row in 0..4 do
+      print @bingo_board[row][0]
+      for place in 1..4 do
+        print " , "
+        print @bingo_board[row][place]
+      end
+      puts
+    end
+   end
 
 end
 
@@ -44,7 +117,10 @@ board = [[47, 44, 71, 8, 88],
         [75, 70, 54, 80, 83]]
 
 new_game = BingoBoard.new(board)
-
+new_game.caller
+new_game.checker
+new_game.mark_correct
+new_game.method
 
 #Reflection
 
