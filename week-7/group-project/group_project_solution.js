462f8abcbd6443f2f0ed9419775227f6ba4649ab
Deanna Warren <mrs.deanna.warren@gmail.com>
2015-10-20 14:43:59 -0700

    Edit file for readability on Github

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,4 +1,7 @@
 /*
 
-As a user, I want to be able to find the sum, median, or mean of any list of numbers, whether it has an even or odd number of items. Each of these should be able to be called on their own, not just with each other.
+As a user, I want to be able to find the sum, median, or mean of any
+list of numbers, whether it has an even or odd number of items. Each
+of these should be able to be called on their own, not just with each
+ other.
 */
