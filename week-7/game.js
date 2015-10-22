571d3ccd1ff9a4a54e8aaa0d3949e9a7fb2b9fb3
Deanna Warren <mrs.deanna.warren@gmail.com>
2015-10-22 11:48:50 -0700

    Add work on solo

--- week-7/game.js
+++ week-7/game.js
@@ -18,6 +18,43 @@
 
 // Pseudocode
 
+/*
+
+PART ONE - story and action (new cat)
+story: Go to animal shelter, pick out a cat.
+action: set a variable catName equal to the user input when asked
+  for the name of the new cat.
+story:Worker will visit house in 5 days to see if the cat is a good fit.
+
+PART TWO- setup (set up objects and related variables)
+set up the following things with associated variables
+cat =
+  cat's name is equal to catName from Part One
+  cat's happiness starts at 50%
+bowl =
+  food starts at 100%
+  water starts at 100%
+litterBox =
+  poop starts at 0%
+
+set up the following code blocks
+code block pet does
+  increase happiness by 10%
+code block fillFood does
+  increase food to 110% full
+code block fillWater does
+  increase water to 110% full
+code block cleanLitter does
+  decrease poop to 0%
+code block turnEnd does
+  decrease happiness, food, and water; increase poop
+code block play does (for the number of turns decided)
+  ask for user input
+  perform appropriate function
+  shows current information
+  run turnEnd code block
+
+*/
 
 // Initial Code
 
@@ -35,7 +72,7 @@
 
 // Reflection
 //
-//
+//https://repl.it/BSuP
 //
 //
 //
