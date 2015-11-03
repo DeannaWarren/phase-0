16cb1998e60a5db3bb516cfb7e1b8c44a9e7cc3b
Deanna Warren <mrs.deanna.warren@gmail.com>
2015-11-03 11:55:14 -0800

    Add week9 peer 1st peer pair file

---
+++ week-9/dom-manipulation/home-page.js
@@ -0,0 +1,42 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge [by myself, with: ].
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+
+
+document.getElementById("release-0").setAttribute("class","done");
+
+
+// Release 1:
+
+document.getElementById("release-1").style.display = "none";
+
+// Release 2:
+
+
+document.getElementsByTagName("h1").item(0).innerHTML = "I completed release 2.";
+
+// Release 3:
+
+
+document.getElementById("release-3").style.backgroundColor = "#955251";
+
+
+// Release 4:
+
+
+//document.getElementsByClassName("release-4").style.textSize = "2em";
+var elements = document.getElementsByClassName("release-4");
+for (var i = 0; i < elements.length; i++) {
+  elements.item(i).style.fontSize = "2em";
+};
+
+// Release 5:
+
+var tmpl = document.getElementById('hidden');
+document.body.appendChild(tmpl.content.cloneNode(true));
